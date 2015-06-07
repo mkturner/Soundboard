70253050ec6936e9e54a5e3e4431f8e3ccaa9643
Marvin T <contact@marvinkturner.com>
2015-06-07 12:24:09 -0400

    finished tableView setup, able to play sound on tap.

--- Soundboard/SoundListViewController.swift
+++ Soundboard/SoundListViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SoundListViewController: UIViewController {
+class SoundListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    // tells tableView how many rows to have
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // tells tableView what's in each row
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // create row object (cell)
+        var cell = UITableViewCell()
+        // assign value to cell
+        cell.textLabel!.text = "BRUH"
+        // send cell back to view
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // find location of playable media
+        var soundPath = NSBundle.mainBundle().pathForResource("bruh", ofType: "m4a")
+        // convert location info into usable URL
+        var soundURL = NSURL.fileURLWithPath(soundPath!)
+        // assign URL to audio player
+        self.audioPlayer = AVAudioPlayer(contentsOfURL: soundURL, error: nil)
+        // Play file at the assigned URL
+        self.audioPlayer.play()
     }
-
-
+    
 }
 
